8c3967199cd369eba747b61dd0cc0d435c0e4886
hafnern <hafnern@540f685e-582c-4437-9ed7-4ccfa8412681>
2017-03-05 19:15:36 +0000

    Started working on things, not done yet, though. Several things such as writing and boolean ops will not work at all.
    git-svn-id: https://svn.inf.ethz.ch/svn/trg/cd_students/2017ss/teams/dirkolas@58989 540f685e-582c-4437-9ed7-4ccfa8412681

--- HW1/src/cd/backend/codegen/ExprGenerator.java
+++ HW1/src/cd/backend/codegen/ExprGenerator.java
@@ -24,116 +24,165 @@ import cd.util.debug.AstOneLine;
  * String which indicates the register where the result can be found.
  */
 class ExprGenerator extends ExprVisitor<Register, Void> {
-	protected final AstCodeGenerator cg;
+    protected final AstCodeGenerator cg;
 
-	ExprGenerator(AstCodeGenerator astCodeGenerator) {
-		cg = astCodeGenerator;
-	}
+    ExprGenerator(AstCodeGenerator astCodeGenerator) {
+        cg = astCodeGenerator;
+    }
 
-	public Register gen(Expr ast) {
-		return visit(ast, null);
-	}
+    public Register gen(Expr ast) {
+        return visit(ast, null);
+    }
 
-	@Override
-	public Register visit(Expr ast, Void arg) {
-		try {
-			cg.emit.increaseIndent("Emitting " + AstOneLine.toString(ast));
-			return super.visit(ast, null);
-		} finally {
-			cg.emit.decreaseIndent();
-		}
+    @Override
+    public Register visit(Expr ast, Void arg) {
+        try {
+            cg.emit.increaseIndent("Emitting " + AstOneLine.toString(ast));
+            return super.visit(ast, null);
+        } finally {
+            cg.emit.decreaseIndent();
+        }
 
-	}
+    }
 
-	@Override
-	public Register binaryOp(BinaryOp ast, Void arg) {
-		{
-			throw new ToDoException();
-		}
-	}
+    @Override
+    public Register binaryOp(BinaryOp ast, Void arg) {
+        Register left = this.visit(ast.left(), arg);
+        Register right = this.visit(ast.right(), arg);
 
-	@Override
-	public Register booleanConst(BooleanConst ast, Void arg) {
-		{
-			throw new RuntimeException("Not required");
-		}
-	}
+        switch(ast.operator){
+        case B_TIMES:
+            cg.emit.emit("imul", right, left);
+            break;
+        case B_DIV:
+            cg.emit.emit("idiv", right, left);
+            break;
+        case B_MOD:
+            break;
+        case B_PLUS:
+            cg.emit.emit("add", right, left);
+            break;
+        case B_MINUS:
+            cg.emit.emit("sub", right, left);
+            break;
+        case B_AND:
+            cg.emit.emit("and", right, left);
+            break;
+        case B_OR:
+            cg.emit.emit("or", right, left);
+            break;
+        case B_EQUAL:
+        case B_NOT_EQUAL:
+        case B_LESS_THAN:
+        case B_LESS_OR_EQUAL:
+        case B_GREATER_THAN:
+        case B_GREATER_OR_EQUAL:
+        }
 
-	@Override
-	public Register builtInRead(BuiltInRead ast, Void arg) {
-		{
-			throw new ToDoException();
-		}
-	}
+        cg.rm.releaseRegister(right);
+        return left;
+    }
 
-	@Override
-	public Register cast(Cast ast, Void arg) {
-		{
-			throw new RuntimeException("Not required");
-		}
-	}
+    @Override
+    public Register booleanConst(BooleanConst ast, Void arg) {
+        {
+            throw new RuntimeException("Not required");
+        }
+    }
 
-	@Override
-	public Register index(Index ast, Void arg) {
-		{
-			throw new RuntimeException("Not required");
-		}
-	}
+    @Override
+    public Register builtInRead(BuiltInRead ast, Void arg) {
+        Register value = cg.rm.getRegister();
+        // Reserve stack space and load variable address into register
+        cg.emit.emit("subl", "$12", "%esp");
+        cg.emit.emit("leal", "8(%esp)", value);
+        // Preprare stack and call scanf
+        cg.emit.emit("movl", value, "4(%esp)");
+        cg.emit.emit("movl", "$scanfinteger", "0(%esp)");
+        cg.emit.emit("call", cd.Config.SCANF);
+        // Read value out of the stack and free the allocated space
+        cg.emit.emit("movl", "8(%esp)", value);
+        cg.emit.emit("addl", "$12", "%esp");
+        return value;
+    }
 
-	@Override
-	public Register intConst(IntConst ast, Void arg) {
-		{
-			throw new ToDoException();
-		}
-	}
+    @Override
+    public Register cast(Cast ast, Void arg) {
+        {
+            throw new RuntimeException("Not required");
+        }
+    }
 
-	@Override
-	public Register field(Field ast, Void arg) {
-		{
-			throw new RuntimeException("Not required");
-		}
-	}
+    @Override
+    public Register index(Index ast, Void arg) {
+        {
+            throw new RuntimeException("Not required");
+        }
+    }
 
-	@Override
-	public Register newArray(NewArray ast, Void arg) {
-		{
-			throw new RuntimeException("Not required");
-		}
-	}
+    @Override
+    public Register intConst(IntConst ast, Void arg) {
+        Register value = cg.rm.getRegister();
+        cg.emit.emit("movl", "$"+ast.value, value);
+        return value;
+    }
 
-	@Override
-	public Register newObject(NewObject ast, Void arg) {
-		{
-			throw new RuntimeException("Not required");
-		}
-	}
+    @Override
+    public Register field(Field ast, Void arg) {
+        {
+            throw new RuntimeException("Not required");
+        }
+    }
 
-	@Override
-	public Register nullConst(NullConst ast, Void arg) {
-		{
-			throw new RuntimeException("Not required");
-		}
-	}
+    @Override
+    public Register newArray(NewArray ast, Void arg) {
+        {
+            throw new RuntimeException("Not required");
+        }
+    }
 
-	@Override
-	public Register thisRef(ThisRef ast, Void arg) {
-		{
-			throw new RuntimeException("Not required");
-		}
-	}
+    @Override
+    public Register newObject(NewObject ast, Void arg) {
+        {
+            throw new RuntimeException("Not required");
+        }
+    }
 
-	@Override
-	public Register unaryOp(UnaryOp ast, Void arg) {
-		{
-			throw new ToDoException();
-		}
-	}
-	
-	@Override
-	public Register var(Var ast, Void arg) {
-		{
-			throw new ToDoException();
-		}
-	}
+    @Override
+    public Register nullConst(NullConst ast, Void arg) {
+        {
+            throw new RuntimeException("Not required");
+        }
+    }
+
+    @Override
+    public Register thisRef(ThisRef ast, Void arg) {
+        {
+            throw new RuntimeException("Not required");
+        }
+    }
+
+    @Override
+    public Register unaryOp(UnaryOp ast, Void arg) {
+    	Register value = this.visit(ast.arg(), arg);
+    	switch(ast.operator){
+    	case U_BOOL_NOT:
+    		cg.emit.emit("not", value);
+    		break;
+    	case U_MINUS:
+    		cg.emit.emit("neg", value);
+    		break;
+    	case U_PLUS:
+    		break;
+    	}
+    	return value;
+    }
+        
+    @Override
+    public Register var(Var ast, Void arg) {
+        {
+            throw new ToDoException();
+        }
+    }
 
 }
--- HW1/src/cd/backend/codegen/StmtGenerator.java
+++ HW1/src/cd/backend/codegen/StmtGenerator.java
@@ -17,77 +17,121 @@ import cd.util.debug.AstOneLine;
  * Generates code to process statements and declarations.
  */
 class StmtGenerator extends AstVisitor<Register, Void> {
-	protected final AstCodeGenerator cg;
+    protected final AstCodeGenerator cg;
 
-	StmtGenerator(AstCodeGenerator astCodeGenerator) {
-		cg = astCodeGenerator;
-	}
+    StmtGenerator(AstCodeGenerator astCodeGenerator) {
+        cg = astCodeGenerator;
+    }
 
-	public void gen(Ast ast) {
-		visit(ast, null);
-	}
+    public void gen(Ast ast) {
+        visit(ast, null);
+    }
 
-	@Override
-	public Register visit(Ast ast, Void arg) {
-		try {
-			cg.emit.increaseIndent("Emitting " + AstOneLine.toString(ast));
-			return super.visit(ast, arg);
-		} finally {
-			cg.emit.decreaseIndent();
-		}
-	}
+    @Override
+    public Register visit(Ast ast, Void arg) {
+        try {
+            cg.emit.increaseIndent("Emitting " + AstOneLine.toString(ast));
+            return super.visit(ast, arg);
+        } finally {
+            cg.emit.decreaseIndent();
+        }
+    }
 
-	@Override
-	public Register methodCall(MethodCall ast, Void dummy) {
-		{
-			throw new RuntimeException("Not required");
-		}
-	}
+    @Override
+    public Register methodCall(MethodCall ast, Void dummy) {
+        {
+            throw new RuntimeException("Not required");
+        }
+    }
 
-	@Override
-	public Register methodDecl(MethodDecl ast, Void arg) {
-		{
-			// Because we only handle very simple programs in HW1,
-			// you can just emit the prologue here!
-			throw new ToDoException();
-		}
-	}
+    @Override
+    public Register methodDecl(MethodDecl ast, Void arg) {
+        {
+            // Because we only handle very simple programs in HW1,
+            // you can just emit the prologue here!
+            throw new ToDoException();
+        }
+    }
 
-	@Override
-	public Register ifElse(IfElse ast, Void arg) {
-		{
-			throw new RuntimeException("Not required");
-		}
-	}
+    @Override
+    public Register ifElse(IfElse ast, Void arg) {
+        {
+            throw new RuntimeException("Not required");
+        }
+    }
 
-	@Override
-	public Register whileLoop(WhileLoop ast, Void arg) {
-		{
-			throw new RuntimeException("Not required");
-		}
-	}
+    @Override
+    public Register whileLoop(WhileLoop ast, Void arg) {
+        {
+            throw new RuntimeException("Not required");
+        }
+    }
 
-	@Override
-	public Register assign(Assign ast, Void arg) {
-		{
-			// Because we only handle very simple programs in HW1,
-			// you can just emit the prologue here!
-			throw new ToDoException();
-		}
-	}
+    @Override
+    public Register assign(Assign ast, Void arg) {
+        {
+            // Because we only handle very simple programs in HW1,
+            // you can just emit the prologue here!
+            throw new ToDoException();
+        }
+    }
 
-	@Override
-	public Register builtInWrite(BuiltInWrite ast, Void arg) {
-		{
-			throw new ToDoException();
-		}
-	}
+    private Register printf(String format, T...args){
+        // Transform format string into word sized chunks of bytes.
+        int[] words = new int[format.length/4+2];
+        int space = words.length+2;
+        for(int i=0; i<words.length; i++){
+            
+        }
+        Register retval = cg.rm.getRegister();
+        cg.emit.emit("movl", "%eax", retval);
+        // Prepare stack and load up the format string.
+        cg.emit.emit("subl", "$"+space, "%esp");
+        for(int i=0; i<words.length; i++){
+            cg.emit.emit("movl", "$"+words[i], (4*i)+"(%esp)");
+        }
+        // Load up the 
+        for(int i=0; i<args.length; i++){
+            cg.emit.emit("movl", args[i], "(%esp)");
+        }
+        cg.emit.emit("call", cd.Config.PRINTF);
+        // Clean up and return.
+        cg.emit.emit("addl", "$"+space, "%esp");
+        cg.emit.emit("xchg", "%eax", retval);
+        return retval;
+    }
 
-	@Override
-	public Register builtInWriteln(BuiltInWriteln ast, Void arg) {
-		{
-			throw new ToDoException();
-		}
-	}
+    @Override
+    public Register builtInWrite(BuiltInWrite ast, Void arg) {
+        // Save EAX in case it's used
+        Register retval = cg.rm.getRegister();
+        cg.emit.emit("movl", "%eax", retval);
+        // Perform the call as usual
+        Register value = this.visit(ast.arg(), arg);
+        cg.emit.emit("subl", "$8", "%esp");
+        cg.emit.emit("movl", value, "4(%esp)");
+        cg.emit.emit("movl", "$printfinteger", "0(%esp)");
+        cg.emit.emit("call", cd.Config.PRINTF);
+        cg.emit.emit("addl", "$8", "%esp");
+        cg.rm.releaseRegister(value);
+        // Swap out the return value in EAX
+        cg.emit.emit("xchg", retval, "%eax");
+        return retval;
+    }
+
+    @Override
+    public Register builtInWriteln(BuiltInWriteln ast, Void arg) {
+        // Save EAX in case it's used
+        Register retval = cg.rm.getRegister();
+        cg.emit.emit("movl", "%eax", retval);
+        // Perform the call as usual
+        cg.emit.emit("subl", "$4", "%esp");
+        cg.emit.emit("movl", "$printfnewline", "0(%esp)");
+        cg.emit.emit("call", cd.Config.PRINTF);
+        cg.emit.emit("addl", "$4", "%esp");
+        // Swap out the return value in EAX
+        cg.emit.emit("xchg", retval, "%eax");
+        return value;
+    }
 
 }
