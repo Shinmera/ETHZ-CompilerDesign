0ca04f4645e133a7411adae423a025c6bd28f6a7
hafnern <hafnern@540f685e-582c-4437-9ed7-4ccfa8412681>
2017-03-29 13:36:53 +0000

    Cleanup formatting
    git-svn-id: https://svn.inf.ethz.ch/svn/trg/cd_students/2017ss/teams/dirkolas@60259 540f685e-582c-4437-9ed7-4ccfa8412681

--- HW2/src/cd/frontend/parser/JavaliAstVisitor.java
+++ HW2/src/cd/frontend/parser/JavaliAstVisitor.java
@@ -26,212 +26,212 @@ import cd.util.Pair;
 
 public final class JavaliAstVisitor extends JavaliBaseVisitor<Void> {
 
-	public List<ClassDecl> classDecls = new ArrayList<>();
-	public List<Decl> decls = new ArrayList<>();
-	public List<VarDecl> varDecls = new ArrayList<>();
-	public List<MethodDecl> methodDecls = new ArrayList<>();
-	public List<Pair<String>> formalParameters = new ArrayList<Pair<String>>();
+    public List<ClassDecl> classDecls = new ArrayList<>();
+    public List<Decl> decls = new ArrayList<>();
+    public List<VarDecl> varDecls = new ArrayList<>();
+    public List<MethodDecl> methodDecls = new ArrayList<>();
+    public List<Pair<String>> formalParameters = new ArrayList<Pair<String>>();
 
-	@Override
-	public Void visitClassDecl(ClassDeclContext ctx) {
+    @Override
+    public Void visitClassDecl(ClassDeclContext ctx) {
 
-		// Superclass.
-		String sClass = "Object";
+        // Superclass.
+        String sClass = "Object";
 
-		/*
-		 * 1. Get Tokens
-		 */
-		// Class name
-		TerminalNode name = ctx.Identifier(0);
+        /*
+         * 1. Get Tokens
+         */
+        // Class name
+        TerminalNode name = ctx.Identifier(0);
 
-		// Superclass
-		TerminalNode superclass = ctx.Identifier(1);
-		if (superclass != null) {
-			sClass = superclass.getText();
-		}
+        // Superclass
+        TerminalNode superclass = ctx.Identifier(1);
+        if (superclass != null) {
+            sClass = superclass.getText();
+        }
 
-		// Get the list of all members.
-		for (DeclarationContext c : ctx.declaration()) {
-			c.accept(this);
-		}
+        // Get the list of all members.
+        for (DeclarationContext c : ctx.declaration()) {
+            c.accept(this);
+        }
 
-		/*
-		 * 2. Create AST
-		 */
-		/*
-		 * // Test VarDecl decl2 = new VarDecl("int", "i"); VarDecl decl3 = new
-		 * VarDecl("int", "j"); List <Decl> decls2 = new ArrayList<Decl>();
-		 * decls2.add(decl2); decls2.add(decl3);
-		 */
+        /*
+         * 2. Create AST
+         */
+        /*
+         * // Test VarDecl decl2 = new VarDecl("int", "i"); VarDecl decl3 = new
+         * VarDecl("int", "j"); List <Decl> decls2 = new ArrayList<Decl>();
+         * decls2.add(decl2); decls2.add(decl3);
+         */
 
-		// TODO decls List<Decl> not working. See ClassDecl definition of the
-		// field 'members'.
-		Ast.ClassDecl decl = new Ast.ClassDecl(name.getText(), sClass, decls);
-		classDecls.add(decl);
+        // TODO decls List<Decl> not working. See ClassDecl definition of the
+        // field 'members'.
+        Ast.ClassDecl decl = new Ast.ClassDecl(name.getText(), sClass, decls);
+        classDecls.add(decl);
 
-		return null;
-	}
+        return null;
+    }
 
-	@Override
-	public Void visitDeclaration(DeclarationContext ctx) {
+    @Override
+    public Void visitDeclaration(DeclarationContext ctx) {
 
-		// List<Decl> list = new ArrayList<Ast.Decl>();
+        // List<Decl> list = new ArrayList<Ast.Decl>();
 
-		// Variable
-		ctx.variableDeclaration().accept(this);
+        // Variable
+        ctx.variableDeclaration().accept(this);
 
-		// Method
-		ctx.methodDeclaration().accept(this);
+        // Method
+        ctx.methodDeclaration().accept(this);
 
-		decls.addAll(varDecls);
-		decls.addAll(methodDecls);
+        decls.addAll(varDecls);
+        decls.addAll(methodDecls);
 
-		return null;
+        return null;
 
-	}
+    }
 
-	@Override
-	public Void visitVariableDeclaration(VariableDeclarationContext ctx) {
+    @Override
+    public Void visitVariableDeclaration(VariableDeclarationContext ctx) {
 
-		// TEST for test case 6.
-		/*
-		 * Ast.VarDecl varDecl = new Ast.VarDecl("int", "i");
-		 * varDecls.add(varDecl);
-		 */
+        // TEST for test case 6.
+        /*
+         * Ast.VarDecl varDecl = new Ast.VarDecl("int", "i");
+         * varDecls.add(varDecl);
+         */
 
-		// Type
-		TerminalNode type = ctx.Type();
+        // Type
+        TerminalNode type = ctx.Type();
 
-		// Identifiers
-		List<TerminalNode> identifiers = ctx.Identifier();
-		
-		// Add to list.
-		for (TerminalNode t : identifiers) {
-			varDecls.add(new Ast.VarDecl(type.getText(), t.getText()));
-		}
+        // Identifiers
+        List<TerminalNode> identifiers = ctx.Identifier();
+                
+        // Add to list.
+        for (TerminalNode t : identifiers) {
+            varDecls.add(new Ast.VarDecl(type.getText(), t.getText()));
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	@Override
-	public Void visitMethodDeclaration(MethodDeclarationContext ctx) {
+    @Override
+    public Void visitMethodDeclaration(MethodDeclarationContext ctx) {
 
-		// return type
-		TerminalNode returnType = ctx.ReturnType();
+        // return type
+        TerminalNode returnType = ctx.ReturnType();
 
-		// identifier
-		TerminalNode name = ctx.Identifier();
+        // identifier
+        TerminalNode name = ctx.Identifier();
 
-		// Parameterlist
-		ctx.formalParameterList().accept(this);
+        // Parameterlist
+        ctx.formalParameterList().accept(this);
 
-		// Variable declarations.
-		for (VariableDeclarationContext c : ctx.variableDeclaration()) {
-			c.accept(this);
-		}
+        // Variable declarations.
+        for (VariableDeclarationContext c : ctx.variableDeclaration()) {
+            c.accept(this);
+        }
 
-		// Statements.
-		for (StatementContext c : ctx.statement()) {
-			c.accept(this);
-		}
+        // Statements.
+        for (StatementContext c : ctx.statement()) {
+            c.accept(this);
+        }
 
-		// Add to list.
+        // Add to list.
 
-		return null;
-	}
+        return null;
+    }
 
-	@Override
-	public Void visitADD(@NotNull JavaliParser.ADDContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitADD(@NotNull JavaliParser.ADDContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitSUB(@NotNull JavaliParser.SUBContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitSUB(@NotNull JavaliParser.SUBContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitExpression(@NotNull JavaliParser.ExpressionContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitExpression(@NotNull JavaliParser.ExpressionContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitOR(@NotNull JavaliParser.ORContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitOR(@NotNull JavaliParser.ORContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitAssignment(@NotNull JavaliParser.AssignmentContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitAssignment(@NotNull JavaliParser.AssignmentContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitNewExpression(@NotNull JavaliParser.NewExpressionContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitNewExpression(@NotNull JavaliParser.NewExpressionContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitFormalParameterList(@NotNull JavaliParser.FormalParameterListContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitFormalParameterList(@NotNull JavaliParser.FormalParameterListContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitINT(@NotNull JavaliParser.INTContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitINT(@NotNull JavaliParser.INTContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitREAD(@NotNull JavaliParser.READContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitREAD(@NotNull JavaliParser.READContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitDIV(@NotNull JavaliParser.DIVContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitDIV(@NotNull JavaliParser.DIVContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitUnit(@NotNull JavaliParser.UnitContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitUnit(@NotNull JavaliParser.UnitContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitNOT(@NotNull JavaliParser.NOTContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitNOT(@NotNull JavaliParser.NOTContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitIDENT(@NotNull JavaliParser.IDENTContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitIDENT(@NotNull JavaliParser.IDENTContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitMULT(@NotNull JavaliParser.MULTContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitMULT(@NotNull JavaliParser.MULTContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitBOOL(@NotNull JavaliParser.BOOLContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitBOOL(@NotNull JavaliParser.BOOLContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitAND(@NotNull JavaliParser.ANDContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitAND(@NotNull JavaliParser.ANDContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitStatement(@NotNull JavaliParser.StatementContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitStatement(@NotNull JavaliParser.StatementContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitUNARY(@NotNull JavaliParser.UNARYContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitUNARY(@NotNull JavaliParser.UNARYContext ctx) {
+        return visitChildren(ctx);
+    }
 
-	@Override
-	public Void visitWrite(@NotNull JavaliParser.WriteContext ctx) {
-		return visitChildren(ctx);
-	}
+    @Override
+    public Void visitWrite(@NotNull JavaliParser.WriteContext ctx) {
+        return visitChildren(ctx);
+    }
 }
